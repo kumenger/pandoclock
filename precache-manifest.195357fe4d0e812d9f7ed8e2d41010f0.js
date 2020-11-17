53d9f86d96fbb87694bdb462f998d286c8672d5e
kumenger <44558777+kumenger@users.noreply.github.com>
2020-11-17 22:52:21 +0300

    Updates

---
+++ precache-manifest.195357fe4d0e812d9f7ed8e2d41010f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcbc5a2e2a1603a35a497842bc18d5d",
+    "url": "/pandoclock/index.html"
+  },
+  {
+    "revision": "48acf1d6f9ddaadf743e",
+    "url": "/pandoclock/static/js/2.8a79be51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pandoclock/static/js/2.8a79be51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5e2d0178ab86010e5b",
+    "url": "/pandoclock/static/js/main.13e6b997.chunk.js"
+  },
+  {
+    "revision": "a1091e94a76b135acb81",
+    "url": "/pandoclock/static/js/runtime-main.8bfa3dd1.js"
+  }
+]);
